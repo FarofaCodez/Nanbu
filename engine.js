21d54f81f27b79d054ab5f1e66a2ac68077d5041
FarofaCodez <anaj42863@outlook.com>
2026-03-28 20:30:38 -0300

    move some stuff into the engine

--- engine.js
+++ engine.js
@@ -71,3 +71,51 @@ class Item {
 
 currentInteraction = () => {};
 let previousInteraction = "";
+
+function characterBillboard(position, character, interactionText = "") {
+	if (interactionText !== "") {
+		ctx.font = "bold 16px sans-serif";
+		ctx.fillStyle = "black";
+		ctx.fillText(character, position.x, position.y - 26);
+		ctx.font = "14px sans-serif";
+		ctx.fillText(`[E] ${interactionText}`, position.x, position.y - 10);
+	} else {
+		ctx.font = "bold 16px sans-serif";
+		ctx.fillStyle = "black";
+		ctx.fillText(character, position.x, position.y - 10);
+	}
+}
+
+let lastTime = 0;
+
+let update = (timestamp) => {};
+
+function render(timestamp) {
+	if (lastTime === 0) {
+		lastTime = timestamp;
+	}
+	const deltaTime = timestamp - lastTime;
+	lastTime = timestamp;
+
+	player.pos.x += (movement.x * player.speed * deltaTime);
+	player.pos.y += (movement.y * player.speed * deltaTime);
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	for (let i = 0; i < objects.length; i++) {
+		if (objects[i].uniqueId !== player.uniqueId) {
+			objects[i].draw();
+		}
+	}
+	
+	ctx.fillStyle = player.color;
+	ctx.fillRect(center.x, center.y, player.width, player.height);
+
+	ctx.fillStyle = "black";
+	ctx.font = "bold 16px sans-serif";
+	ctx.fillText("Money: " + player.money, 12, 20);
+
+	update(timestamp);
+	
+	requestAnimationFrame(render);
+}
+requestAnimationFrame(render);
